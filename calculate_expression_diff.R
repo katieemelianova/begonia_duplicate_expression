8de6aa45d10fd3e0fc0ad8b43f7935aa2a0aaef2
katieemelianova <katie_emelianova@hotmail.com>
2021-02-23 18:48:41 +0000

    rewrite plot paralogs function

--- calculate_expression_diff.R
+++ calculate_expression_diff.R
@@ -7,56 +7,70 @@ library(wCorr)
 
 
 
-# give full dataframe of expression and an orthogroup to plot
-plot_paralogs<-function(df, og){
-  test_set<-df %>% filter(orthogroup == og)
-  c1<-test_set$counts[1:6]
-  c2<-test_set$counts[7:12]
-  p1<-test_set$counts[13:18]
-  p2<-test_set$counts[19:24]
-  up_lim<-max(test_set$counts) +1
-  low_lim<-min(test_set$counts) -1
-  plot(c1, col="red", type='l', xaxt="n", ylim = c(low_lim, up_lim), pch=16)
-  axis(1, at=1:6, labels=c("fflower", "leaf", "mflower", "petiole", "root", "vegbud"))
-  lines(c2, col="red", pch=16)
-  lines(p1, col="blue", pch=16)
-  lines(p2, col="blue", pch=16)s
-}
+
+
+con_counts %>% 
+  filter(rowname == as.character("CON_TRINITY_DN6298_c1_g2_i2")) %>% 
+  select(CONfemaleFlower, CONleaf, CONmaleFlower, CONpetiole, CONroot, CONvegBud) %>% 
+  as.character() %>%
+  as.numeric()
+
+
+con_counts %>% filter(rowname == "CON_TRINITY_DN6687_c3_g3_i1") %>% select(CONfemaleFlower, CONleaf, CONmaleFlower, CONpetiole, CONroot, CONvegBud)
+
 
 ortho<-read_tsv("/Users/katie/Desktop/Bg/begonia_duplicate_expression/Orthogroups/Orthogroups.tsv")
 colnames(ortho)<-c("orthogroup", "con", "ple")
 
 # filter out only rows where two copies present in both CON and PLE
 # separate the string of two contigs into two separate columns
-x<-ortho %>%
+orthogroups<-ortho %>%
   filter(str_count(con, "CON") == 2) %>% 
   filter(str_count(ple, "PLE") == 2) %>%
   separate(con, c("con1", "con2"), sep=", ") %>% 
   separate(ple, c("ple1", "ple2"), sep=", ")
 
 # edit names of trinity contigs to match the ones in the counts files
-x$con1<-x$con1 %>% str_remove(".p1")
-x$con2<-x$con2 %>% str_remove(".p1")
-x$ple1<-x$ple1 %>% str_remove(".p1")
-x$ple2<-x$ple2 %>% str_remove(".p1")
+orthogroups$con1<-orthogroups$con1 %>% str_remove(".p1")
+orthogroups$con2<-orthogroups$con2 %>% str_remove(".p1")
+orthogroups$ple1<-orthogroups$ple1 %>% str_remove(".p1")
+orthogroups$ple2<-orthogroups$ple2 %>% str_remove(".p1")
 
-con_counts<-read.table("con_avg_cpm")
-ple_counts<-read.table("ple_avg_cpm")
-
-# make the rownames an explicit variable
-con_counts<-con_counts %>% tibble::rownames_to_column()
-ple_counts<-ple_counts %>% tibble::rownames_to_column()
+con_counts<-read.table("con_avg_cpm") %>% tibble::rownames_to_column()
+ple_counts<-read.table("ple_avg_cpm") %>% tibble::rownames_to_column()
 
 con_counts$rowname <- con_counts$rowname %>% str_remove(".p1")
 ple_counts$rowname <- ple_counts$rowname %>% str_remove(".p1")
 
 
+plot_paralogs<-function(og){
+  con1<-orthogroups %>% filter(orthogroup == og) %>% select(con1)
+  con2<-orthogroups %>% filter(orthogroup == og) %>% select(con2)
+  ple1<-orthogroups %>% filter(orthogroup == og) %>% select(ple1)
+  ple2<-orthogroups %>% filter(orthogroup == og) %>% select(ple2)
+  
+  con1_ex <- con_counts %>% filter(rowname == as.character(con1)) %>% select(CONfemaleFlower, CONleaf, CONmaleFlower, CONpetiole, CONroot, CONvegBud) %>% as.character() %>% as.numeric() 
+  con2_ex <- con_counts %>% filter(rowname == as.character(con2)) %>% select(CONfemaleFlower, CONleaf, CONmaleFlower, CONpetiole, CONroot, CONvegBud) %>% as.character() %>% as.numeric() 
+  ple1_ex <- ple_counts %>% filter(rowname == as.character(ple1)) %>% select(PLEfemaleFlower, PLEleaf, PLEmaleFlower, PLEpetiole, PLEroot, PLEvegBud) %>% as.character() %>% as.numeric() 
+  ple2_ex <- ple_counts %>% filter(rowname == as.character(ple2)) %>% select(PLEfemaleFlower, PLEleaf, PLEmaleFlower, PLEpetiole, PLEroot, PLEvegBud) %>% as.character() %>% as.numeric() 
+  
+  up_lim<-max(c(con1_ex, con2_ex, ple1_ex, ple2_ex)) +1
+  low_lim<-min(c(con1_ex, con2_ex, ple1_ex, ple2_ex)) -1
+  
+  plot(con1_ex, col="red", type='l', xaxt="n", ylim = c(low_lim, up_lim), pch=16, xlab="Tissue", ylab="FPKM")
+  axis(1, at=1:6, labels=c("fflower", "leaf", "mflower", "petiole", "root", "vegbud"))
+  lines(ple1_ex, col="red", pch=16)
+  lines(con2_ex, col="blue", pch=16)
+  lines(ple2_ex, col="blue", pch=16)
+}
+
+
 ##########################################################################################
 ##########################################################################################
 
 
 
-getit_realthing<-function(l){
+calculate_difference<-function(l){
   c1<-con_counts %>% filter(rowname %in% l$con1)
   c2<-con_counts %>% filter(rowname %in% l$con2)
   
@@ -77,15 +91,11 @@ getit_realthing<-function(l){
     to_return_list[["ple1_expr"]]<-p1[,2:7]
     to_return_list[["ple2_expr"]]<-p2[,2:7]
     
-    c1<-c1[,2:7]
-    c2<-c2[,2:7]
-    c1<-c1 %>% as.character() %>% as.numeric()
-    c2<-c2 %>% as.character() %>% as.numeric()
-    p1<-p1[,2:7]
-    p2<-p2[,2:7]
-    p1<-p1 %>% as.character() %>% as.numeric()
-    p2<-p2 %>% as.character() %>% as.numeric()
-    
+    c1<-c1[,2:7] %>% as.character() %>% as.numeric()
+    c2<-c2[,2:7] %>% as.character() %>% as.numeric()
+    p1<-p1[,2:7] %>% as.character() %>% as.numeric()
+    p2<-p2[,2:7] %>% as.character() %>% as.numeric()
+
     # get difference between paralogs at each tissue
     con_diff<- (abs(c1 - c2))^2
     ple_diff<- (abs(p1 - p2))^2
@@ -94,10 +104,7 @@ getit_realthing<-function(l){
     ple_diff_sum <- sum(ple_diff)
     con_maxdiff <- max(con_diff)
     ple_maxdiff <- max(ple_diff)
-    
-    to_return_list[["con_maxdiff"]] <- con_maxdiff
-    to_return_list[["ple_maxdiff"]] <- ple_maxdiff
-    
+
     # get ratio of largest sum of differences to smallest sum of differences
     sums_ratio<-max(c(con_diff_sum, ple_diff_sum))/min(c(con_diff_sum, ple_diff_sum))
     
@@ -106,58 +113,146 @@ getit_realthing<-function(l){
     
     to_return_list[["max_diff_ratio"]] <- max_diff_ratio
     to_return_list[["sums_ratio"]] <- sums_ratio
-    
-    to_return_list[["con_sumdiffs"]] <- con_diff_sum
-    to_return_list[["ple_sumdiffs"]] <- ple_diff_sum
-    
   }
   return(to_return_list)
 }
 
 
-for (i in 1:length(rownames(x))){
-  con_ple_list<-getit_realthing(x[i,])
 
-  if (!is.null(con_ple_list[["con_sumdiffs"]]) & !is.null(con_ple_list[["ple_sumdiffs"]])) { 
-    a<-cbind(con_ple_list[["con1_expr"]], con_ple_list[["con2_expr"]], con_ple_list[["ple1_expr"]], con_ple_list[["ple2_expr"]])
-    a<-a %>% as.character() %>% as.numeric()
+diffs_df<-data.frame(orthogroup=c(), sumdiffs=c(), maxdiffs<-c())
+plot(log(diffs_df$sumdiffs), log(diffs_df$maxdiffs))
+
+
+
+
+# orthogroups whete maxdiff is > 3
+plot_paralogs("OG0000529")
+plot_paralogs("OG0000553")
+plot_paralogs("OG0000587")
+plot_paralogs("OG0000598")
+plot_paralogs("OG0000605")
+plot_paralogs("OG0000606")
+plot_paralogs("OG0000619")
+plot_paralogs("OG0000622")
+plot_paralogs("OG0000624")
+plot_paralogs("OG0000625")
+plot_paralogs("OG0000643")
+plot_paralogs("OG0000651")
+plot_paralogs("OG0000654")
+plot_paralogs("OG0000666")
+plot_paralogs("OG0000672")
+plot_paralogs("OG0000682")
+plot_paralogs("OG0000685")
+plot_paralogs("OG0000689")
+plot_paralogs("OG0000697")
+plot_paralogs("OG0000703")
+plot_paralogs("OG0000712")
+plot_paralogs("OG0000733")
+plot_paralogs("OG0000737")
+plot_paralogs("OG0000747")
+plot_paralogs("OG0000749")
+plot_paralogs("OG0000753")
+plot_paralogs("OG0000765")
+plot_paralogs("OG0000769")
+plot_paralogs("OG0000784")
+plot_paralogs("OG0000799")
+plot_paralogs("OG0000805")
+plot_paralogs("OG0000807")
+plot_paralogs("OG0000837")
+plot_paralogs("OG0000861")
+plot_paralogs("OG0000862")
+plot_paralogs("OG0000887")
+plot_paralogs("OG0000908")
+plot_paralogs("OG0000915")
+plot_paralogs("OG0000933")
+plot_paralogs("OG0000958")
+plot_paralogs("OG0000961")
+plot_paralogs("OG0000963")
+plot_paralogs("OG0000966")
+plot_paralogs("OG0000998")
+plot_paralogs("OG0001005")
+plot_paralogs("OG0001014")
+plot_paralogs("OG0001015")
+plot_paralogs("OG0001018")
+plot_paralogs("OG0001030")
+plot_paralogs("OG0001035")
+plot_paralogs("OG0001045")
+plot_paralogs("OG0001048")
+plot_paralogs("OG0001052")
+plot_paralogs("OG0001053")
+plot_paralogs("OG0001060")
+plot_paralogs("OG0001071")
+plot_paralogs("OG0001074")
+plot_paralogs("OG0001087")
+plot_paralogs("OG0001091")
+plot_paralogs("OG0001095")
+plot_paralogs("OG0001098")
+plot_paralogs("OG0001105")
+plot_paralogs("OG0001106")
+plot_paralogs("OG0001115")
+plot_paralogs("OG0001119")
+plot_paralogs("OG0001121")
+plot_paralogs("OG0001137")
+plot_paralogs("OG0001141")
+plot_paralogs("OG0001143")
+plot_paralogs("OG0001148")
+plot_paralogs("OG0001156")
+plot_paralogs("OG0001171")
+plot_paralogs("OG0001179")
+plot_paralogs("OG0001184")
+plot_paralogs("OG0001186")
+plot_paralogs("OG0001196")
+plot_paralogs("OG0001200")
+plot_paralogs("OG0001202")
+plot_paralogs("OG0001205")
+plot_paralogs("OG0001209")
+plot_paralogs("OG0001220")
+plot_paralogs("OG0001229")
+plot_paralogs("OG0001247")
+plot_paralogs("OG0001272")
+plot_paralogs("OG0001280")
+plot_paralogs("OG0001294")
+plot_paralogs("OG0001297")
+plot_paralogs("OG0001301")
+plot_paralogs("OG0001304")
+plot_paralogs("OG0001308")
+plot_paralogs("OG0001309")
+plot_paralogs("OG0001337")
+plot_paralogs("OG0001338")
+plot_paralogs("OG0001340")
+plot_paralogs("OG0001355")
+
+
+diffs_df %>% filter(maxdiffs > 3) %>% select(orthogroup)
+
+for (i in 1:length(rownames(orthogroups))){
+  con_ple_list<-calculate_difference(orthogroups[i,])
+
+  if (!is.null(con_ple_list[["max_diff_ratio"]]) & !is.null(con_ple_list[["sums_ratio"]])) { 
     
-    cint<-con_ple_list[["con_sumdiffs"]] %>% round(0)
-    pint<-con_ple_list[["ple_sumdiffs"]] %>% round(0)
-    
-    crange<-con_ple_list[["con_maxdiff"]] %>% round(0)
-    prange<-con_ple_list[["ple_maxdiff"]] %>% round(0)
-    
+    con1_exp<-con_ple_list[["con1_expr"]] %>% as.character() %>% as.numeric()
+    con2_exp<-con_ple_list[["con2_expr"]] %>% as.character() %>% as.numeric()
+    ple1_exp<-con_ple_list[["ple1_expr"]] %>% as.character() %>% as.numeric()
+    ple2_exp<-con_ple_list[["ple2_expr"]] %>% as.character() %>% as.numeric()
+    orthogroup<-con_ple_list[["orthogroup"]]                      
     max_diff_ratio<-con_ple_list[["max_diff_ratio"]] %>% round(1)
     sums_ratio<-con_ple_list[["sums_ratio"]] %>% round(1)
 
-    ortho<-con_ple_list[["orthogroup"]]
-    
-    
-    diff_exspr<-paste(sums_ratio, max_diff_ratio, collapse="      ")
-    colours<-c(rep("red", 12), rep("blue", 12))
-    up_lim<-max(a) +1
-    low_lim<-min(a) -1
-    #pdf(plot_name)
-    plot(a[1:6], pch=8, type="l", xaxt='n', main=diff_exspr, col="red", ylim = c(low_lim, up_lim), xlab="Tissue", ylab="FPKM")
-    axis(1, at=1:6, labels=c("fflower", "leaf", "mflower", "petiole", "root", "vegbud"))
-    #legend("bottomright", legend=c("conchifolia", "plebeja"), col=c("red", "blue"), lty=1:2, cex=0.8)
-    lines(a[7:12], col="red")
-    lines(a[13:18], lty="dashed", col="blue")
-    lines(a[19:24], lty="dashed", col="blue") 
-    #dev.off()
-    Sys.sleep(6)
-    #plot_number<-plot_number+1
+    mydiffs<-data.frame(orthogroup=orthogroup, sumdiffs=sums_ratio, maxdiffs=max_diff_ratio)
+    diffs_df<-rbind(diffs_df, mydiffs)
+
+    #diff_exspr<-paste(sums_ratio, max_diff_ratio, collapse="      ")
+    #up_lim<-max(c(con1_exp, con2_exp, ple1_exp, ple2_exp)) +1
+    #low_lim<-min(c(con1_exp, con2_exp, ple1_exp, ple2_exp)) -1
+    #plot(con1_exp, pch=8, type="l", xaxt='n', main=diff_exspr, col="red", ylim = c(low_lim, up_lim), xlab="Tissue", ylab="FPKM")
+    #axis(1, at=1:6, labels=c("fflower", "leaf", "mflower", "petiole", "root", "vegbud"))
+    #lines(con2_exp, col="red")
+    #lines(ple1_exp, lty="dashed", col="blue")
+    #lines(ple2_exp, lty="dashed", col="blue") 
+    #Sys.sleep(3)
   }
 }
-
-
-plot_paralogs(total_df, "OG0000587")
-plot_paralogs(total_df, "OG0000598")
-plot_paralogs(total_df, "OG0000624")
-plot_paralogs(total_df, "OG0000643")
-plot_paralogs(total_df, "OG0000654")
-
+ 
 
 
 
