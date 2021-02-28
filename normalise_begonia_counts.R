6093d66d390dd9b2f55f2a4ee9ad3662972226cc
katieemelianova <katie_emelianova@hotmail.com>
2021-02-28 10:38:58 +0000

    add normalisation code

---
+++ normalise_begonia_counts.R
@@ -0,0 +1,111 @@
+library(edgeR)
+#library(baySeq)
+
+#read in featurecounts output
+con_counts<-read.table("/Users/katie/Desktop/Bg/begonia_get_fpkms/featurecount/con/con_counts", header=TRUE)
+ple_counts<-read.table("/Users/katie/Desktop/Bg/begonia_get_fpkms/featurecount/ple/ple_counts", header=TRUE)
+
+
+# gen the gene IDs (these are unique, unlike the Chr column)
+# make them shorter, chopping off front diuplicated naming pattern
+con_geneid<-sapply(con_counts$Geneid, function(x) str_split(x, "~~")[[1]][2]) %>% as.vector()
+ple_geneid<-sapply(ple_counts$Geneid, function(x) str_split(x, "~~")[[1]][2]) %>% as.vector()
+
+# prepend them with CON_ and PLE_ to match that of Orthogroups
+con_names<-paste("CON", con_geneid, sep="_")
+ple_names<-paste("PLE", ple_geneid, sep="_")
+
+# remove all the path names fro  the column names to make them shorter
+con_cols<-colnames(con_counts) %>% 
+  str_remove("X.home.kemelianova.to_download.trimmed_decontaminated.star.") %>% 
+  str_remove("Aligned.sortedByCoord.out.bam")
+
+ple_cols<-colnames(ple_counts) %>% 
+  str_remove("X.home.kemelianova.to_download.trimmed_decontaminated.star.") %>% 
+  str_remove("Aligned.sortedByCoord.out.bam")
+
+# rename columns using the shorter version
+colnames(con_counts)<-con_cols
+colnames(ple_counts)<-ple_cols
+
+con_lengths<-con_counts %>% select(Length) %>% set_rownames(con_names)
+ple_lengths<-ple_counts %>% select(Length) %>% set_rownames(ple_names)
+
+
+# select columns of the counts, excluding chr start end etc info at beginning
+con_counts<-con_counts[,7:23]
+row.names(con_counts)<-con_names
+ple_counts<-ple_counts[,7:23]
+row.names(ple_counts)<- ple_names
+
+# assign groups to the columns in con and ple
+# missing letters are for Con root 2 and Ple petiole 3
+con_groups<-c('A','A','A','B','B','B','C','C','C','D','D','D','E','E','F','F','F')
+ple_groups<-c('G','G','G','H','H','H','I','I','I','J','J','K','K','K','L','L','L')
+
+# create DGE list
+# keep only rows where counts per million is above 100 in at least 2 samples
+# calculate normalisation factors
+# estimate tagwise and common dispersal
+con_d<-DGEList(counts=con_counts,group=factor(con_groups))
+con_d.full<-con_d
+con_d.keep<-rowSums(cpm(con_d)>1)>=2
+con_d<-con_d[con_d.keep,]
+con_d_lengths<-con_lengths[con_d.keep,] %>% data.frame()
+con_d$samples$lib.size<-colSums(con_d$counts)
+con_d<-calcNormFactors(con_d, method="TMM")
+plotMDS(con_d, method="bcv", col=as.numeric(con_d$samples$group))
+con_d<-estimateCommonDisp(con_d)
+con_d<-estimateTagwiseDisp(con_d, prior.n = 4)
+
+
+ple_d<-DGEList(counts= ple_counts,group=factor(ple_groups))
+ple_d.full<-ple_d
+ple_d.keep<-rowSums(cpm(ple_d)>1)>=2
+ple_d<-ple_d[ple_d.keep,]
+ple_d_lengths<-ple_lengths[ple_d.keep,] %>% data.frame()
+rownames(ple_d_lengths)<-rownames(ple_d$counts)
+ple_d$samples$lib.size<-colSums(ple_d$counts)
+ple_d<-calcNormFactors(ple_d, method="TMM")
+plotMDS(ple_d, method="bcv", col=as.numeric(ple_d$samples$group))
+ple_d<-estimateCommonDisp(ple_d)
+ple_d<-estimateTagwiseDisp(ple_d, prior.n = 4)
+
+
+con_d$counts %>% data.frame() %>% 
+  select(CONfemaleFlower1, CONfemaleFlower2, CONfemaleFlower3) %>% 
+  rpkmByGroup(gene.length=con_d_lengths)
+
+CONfemaleFlower<-con_d$counts %>% data.frame() %>% select(CONfemaleFlower1, CONfemaleFlower2, CONfemaleFlower3) %>% rpkmByGroup(gene.length=con_d_lengths)  %>% data.frame()
+CONleaf<-con_d$counts %>% data.frame() %>% select(CONleaf1, CONleaf2, CONleaf3) %>% rpkmByGroup(gene.length=con_d_lengths) %>% data.frame()
+CONmaleFlower<-con_d$counts %>% data.frame() %>% select(CONmaleFlower1, CONmaleFlower2, CONmaleFlower3) %>% rpkmByGroup(gene.length=con_d_lengths) %>% data.frame()
+CONpetiole<-con_d$counts %>% data.frame() %>% select(CONpetiole1, CONpetiole2, CONpetiole3) %>% rpkmByGroup(gene.length=con_d_lengths) %>% data.frame()
+CONroot<-con_d$counts %>% data.frame() %>% select(CONroot1, CONroot3, CONvegBud1) %>% rpkmByGroup(gene.length=con_d_lengths) %>% data.frame()
+CONvegBud<-con_d$counts %>% data.frame() %>% select(CONvegBud1, CONvegBud2, CONvegBud3) %>% rpkmByGroup(gene.length=con_d_lengths) %>% data.frame()
+
+PLEfemaleFlower<-ple_d$counts %>% data.frame() %>% select(PLEfemaleFlower1, PLEfemaleFlower2, PLEfemaleFlower3) %>% rpkmByGroup(gene.length=ple_d_lengths)  %>% data.frame()
+PLEleaf<-ple_d$counts %>% data.frame() %>% select(PLEleaf1, PLEleaf2, PLEleaf3) %>% rpkmByGroup(gene.length=ple_d_lengths)  %>% data.frame()
+PLEmaleFlower<-ple_d$counts %>% data.frame() %>% select(PLEmaleFlower1, PLEmaleFlower2, PLEmaleFlower3) %>% rpkmByGroup(gene.length=ple_d_lengths)  %>% data.frame()
+PLEpetiole<-ple_d$counts %>% data.frame() %>% select(PLEpetiole1, PLEpetiole2) %>% rpkmByGroup(gene.length=ple_d_lengths)  %>% data.frame()
+PLEroot<-ple_d$counts %>% data.frame() %>% select(PLEroot1, PLEroot2, PLEroot3) %>% rpkmByGroup(gene.length=ple_d_lengths) %>% data.frame()
+PLEvegBud<-ple_d$counts %>% data.frame() %>% select(PLEvegBud1, PLEvegBud2, PLEvegBud3) %>% rpkmByGroup(gene.length=ple_d_lengths)  %>% data.frame()
+
+
+# bind con and ple together per species
+con_avg_cpm<-cbind(CONfemaleFlower, CONleaf, CONmaleFlower, CONpetiole, CONroot, CONvegBud)
+ple_avg_cpm<-cbind(PLEfemaleFlower, PLEleaf, PLEmaleFlower, PLEpetiole, PLEroot, PLEvegBud)
+
+# transfer row names
+rownames(con_avg_cpm)<-rownames(con_d$counts)
+rownames(ple_avg_cpm)<-rownames(ple_d$counts)
+
+# assign colnames
+colnames(con_avg_cpm)<-c("CONfemaleFlower", "CONleaf", "CONmaleFlower", "CONpetiole", "CONroot", "CONvegBud")
+colnames(ple_avg_cpm)<-c("PLEfemaleFlower", "PLEleaf", "PLEmaleFlower", "PLEpetiole", "PLEroot", "PLEvegBud")
+
+# write to table
+write.table(con_avg_cpm, file="con_avg_cpm", sep="\t", quote = FALSE, row.names = TRUE)
+write.table(ple_avg_cpm, file="ple_avg_cpm", sep="\t", quote = FALSE, row.names = TRUE)
+
+
+
