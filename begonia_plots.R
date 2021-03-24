13ffb58ae7fe7d6133f54080ee0e55c392f7fdd0
katieemelianova <katie_emelianova@hotmail.com>
2021-03-24 17:13:31 +0000

    clean up code

--- begonia_plots.R
+++ begonia_plots.R
@@ -7,58 +7,24 @@ library(tidyr)
 library(UpSetR)
 library(ggpubr)
 library(pheatmap)
-
-
-###################### orthogroup histogram and bubble plot
-######################
-
-ortho<-read.table("/Users/katie/Desktop/Bg/begonia_duplicate_expression/orthogroup_size_table.txt", header = FALSE)
-colnames(ortho) <- c("og", "c", "p")
-
-a<-ortho %>% dplyr::select(c, p) %>% table() %>% data.frame()
-
-# bubble plot of gene family sizes
-ggplot(a, aes(x=c, y=p, size=Freq)) +
-  geom_point(alpha=0.5) +
-  scale_size(range=c(0, 30))
-
-
-con_orthogroups<-data.frame(count=ortho$c, species="B. conchifolia")
-ple_orthogroups<-data.frame(count=ortho$p, species="B. plebeja")
-orthogroups<-rbind(con_orthogroups, ple_orthogroups)
-
-
-ggplot(orthogroups, aes(x=count, colour=species, fill=species)) + 
-  geom_histogram(alpha=0.8, position = "dodge", bins=11) + 
-  labs(x = "Orthogroup size", y = "Frequency") + 
-  theme(legend.text=element_text(size=15), 
-        legend.title = element_blank(), 
-        axis.title.x=element_text(size=15), 
-        axis.title.y=element_text(size=15),
-        axis.text.x= element_text(size=13),
-        axis.text.y= element_text(size=13))
-
-###################### GO plots
-######################
 library(edgeR)
-#library(baySeq)
+library(GO.db)
+
+
+#########################################################
+###           prepare normalised counts                ##
+###     Do not filter by cpm to include low counts     ##
+#########################################################
 
 #read in featurecounts output
 con_counts<-read.table("/Users/katie/Desktop/Bg/begonia_duplicate_expression/featurecount/con/con_counts", header=TRUE)
 ple_counts<-read.table("/Users/katie/Desktop/Bg/begonia_duplicate_expression/featurecount/ple/ple_counts", header=TRUE)
 
-
-# gen the gene IDs (these are unique, unlike the Chr column)
-# make them shorter, chopping off front diuplicated naming pattern
-con_geneid<-sapply(con_counts$Geneid, function(x) str_split(x, "~~")[[1]][2]) %>% as.vector()
-ple_geneid<-sapply(ple_counts$Geneid, function(x) str_split(x, "~~")[[1]][2]) %>% as.vector()
-
 # prepend them with CON_ and PLE_ to match that of Orthogroups
-con_names<-paste("CON", con_geneid, sep="_")
-ple_names<-paste("PLE", ple_geneid, sep="_")
+con_names<-paste("CON", con_counts$Geneid, sep="_")
+ple_names<-paste("PLE", ple_counts$Geneid, sep="_")
 
-
-# remove all the path names fro  the column names to make them shorter
+# remove all the path names from the column names to make them shorter
 con_cols<-colnames(con_counts) %>% 
   str_remove("X.home.kemelianova.to_download.trimmed_decontaminated.star.") %>% 
   str_remove("Aligned.sortedByCoord.out.bam")
@@ -93,21 +59,18 @@ con_d<-DGEList(counts=con_counts,group=factor(con_groups))
 con_d_lengths<-con_lengths %>% data.frame()
 con_d$samples$lib.size<-colSums(con_d$counts)
 con_d<-calcNormFactors(con_d, method="TMM")
-plotMDS(con_d, method="bcv", col=as.numeric(con_d$samples$group))
-
-
 
 ple_d<-DGEList(counts= ple_counts,group=factor(ple_groups))
 ple_d_lengths<-ple_lengths %>% data.frame()
 rownames(ple_d_lengths)<-rownames(ple_d$counts)
 ple_d$samples$lib.size<-colSums(ple_d$counts)
 ple_d<-calcNormFactors(ple_d, method="TMM")
-plotMDS(ple_d, method="bcv", col=as.numeric(ple_d$samples$group))
 
 
 
-########## 
-# unique GO terms
+#########################################################
+###           get average FPKM per tissue              ##
+#########################################################
 
 
 CONfemaleFlower<-con_d$counts %>% data.frame() %>% select(CONfemaleFlower1, CONfemaleFlower2, CONfemaleFlower3) %>% rpkmByGroup(gene.length=con_d_lengths)  %>% data.frame()
@@ -124,7 +87,6 @@ PLEpetiole<-ple_d$counts %>% data.frame() %>% select(PLEpetiole1, PLEpetiole2) %
 PLEroot<-ple_d$counts %>% data.frame() %>% select(PLEroot1, PLEroot2, PLEroot3) %>% rpkmByGroup(gene.length=ple_d_lengths) %>% data.frame()
 PLEvegBud<-ple_d$counts %>% data.frame() %>% select(PLEvegBud1, PLEvegBud2, PLEvegBud3) %>% rpkmByGroup(gene.length=ple_d_lengths)  %>% data.frame()
 
-
 # bind con and ple together per species
 con_avg_cpm<-cbind(CONfemaleFlower, CONleaf, CONmaleFlower, CONpetiole, CONroot, CONvegBud)
 ple_avg_cpm<-cbind(PLEfemaleFlower, PLEleaf, PLEmaleFlower, PLEpetiole, PLEroot, PLEvegBud)
@@ -138,7 +100,10 @@ colnames(con_avg_cpm)<-c("CONfemaleFlower", "CONleaf", "CONmaleFlower", "CONpeti
 colnames(ple_avg_cpm)<-c("PLEfemaleFlower", "PLEleaf", "PLEmaleFlower", "PLEpetiole", "PLEroot", "PLEvegBud")
 
 
-#rownames(con_avg_cpm) <- str_remove(rownames(con_avg_cpm), ".p1")
+############################################################################
+###     filter features which are expressed > 1 FPKM in *one* tissue       #
+###    all other tissues are expressed at < 1 (i.e. unique to one tissue)  #
+############################################################################
 
 CONfemaleFlower_specific<-con_avg_cpm %>% filter(CONfemaleFlower > 1 & CONroot < 1 & CONpetiole < 1 & CONvegBud < 1 & CONmaleFlower < 1 & CONleaf < 1)
 CONmaleFlower_specific<-con_avg_cpm %>% filter(CONmaleFlower > 1 & CONroot < 1 & CONpetiole < 1 & CONvegBud < 1 & CONleaf < 1 & CONfemaleFlower < 1)
@@ -146,7 +111,6 @@ CONleaf_specific<-con_avg_cpm %>% filter(CONleaf > 1 & CONmaleFlower < 1 & CONro
 CONpetiole_specific<-con_avg_cpm %>% filter(CONpetiole > 1 & CONleaf < 1 & CONmaleFlower < 1 & CONroot < 1 & CONvegBud < 1 & CONfemaleFlower < 1)
 CONroot_specific<-con_avg_cpm %>% filter(CONroot > 1 & CONpetiole < 1 & CONleaf < 1 & CONmaleFlower < 1 & CONvegBud < 1 & CONfemaleFlower < 1)
 CONvegBud_specific<-con_avg_cpm %>% filter(CONvegBud > 1 & CONroot < 1 & CONpetiole < 1 & CONleaf < 1 & CONmaleFlower < 1 & CONfemaleFlower < 1)
-
 
 PLEfemaleFlower_specific<-ple_avg_cpm %>% filter(PLEfemaleFlower > 1 & PLEroot < 1 & PLEpetiole < 1 & PLEvegBud < 1 & PLEmaleFlower < 1 & PLEleaf < 1)
 PLEmaleFlower_specific<-ple_avg_cpm %>% filter(PLEmaleFlower > 1 & PLEroot < 1 & PLEpetiole < 1 & PLEvegBud < 1 & PLEleaf < 1 & PLEfemaleFlower < 1)
@@ -156,6 +120,11 @@ PLEroot_specific<-ple_avg_cpm %>% filter(PLEroot > 1 & PLEpetiole < 1 & PLEleaf 
 PLEvegBud_specific<-ple_avg_cpm %>% filter(PLEvegBud > 1 & PLEroot < 1 & PLEpetiole < 1 & PLEleaf < 1 & PLEmaleFlower < 1 & PLEfemaleFlower < 1)
 
 
+##############################################################################
+###                  Loop through uniquely expressed genes                   #
+###  use to find GO terms associated with that gene (from trinotate output)  #
+##     append these GO terms to GO terms foer that tissue specific GO terms  #
+##############################################################################
 
 con_annotation<-read.table("/Users/katie/Desktop/Bg/begonia_duplicate_expression/download_updated/trinotate/con/con_go_annotations_trans.txt")
 ple_annotation<-read.table("/Users/katie/Desktop/Bg/begonia_duplicate_expression/download_updated/trinotate/ple/ple_go_annotations_trans.txt")
@@ -310,16 +279,8 @@ for (i in rownames(PLEvegBud_specific)){
 }
 
 
-
-
-
-
-
-
-library(GO.db)
-Term("GO:0016021")
-Ontology("GO:0016021")
-
+# a function to plot the top 25 GO terms from tissue specific GO terms
+# splits the GO terms into different ontologies (MF, CC, BP)
 plot_go_graph<-function(input_terms){
   df<-data.frame(table(input_terms), Term(names(table(input_terms))), Ontology(names(table(input_terms))))
   colnames(df)<-c("go_term", "count", "term_name", "ontology")
@@ -335,7 +296,7 @@ plot_go_graph<-function(input_terms){
 }
 
 
-
+# plot each tissue specific GO term graph for B. conchifolia
 a<-plot_go_graph(CONfemaleFlower_specific_goterms)
 b<-plot_go_graph(CONmaleFlower_specific_goterms)
 c<-plot_go_graph(CONleaf_specific_goterms)
@@ -343,7 +304,7 @@ d<-plot_go_graph(CONpetiole_specific_goterms)
 e<-plot_go_graph(CONroot_specific_goterms)
 f<-plot_go_graph(CONvegBud_specific_goterms)
 
-
+# plot each tissue specific GO term graph for B. plebeja
 a<-plot_go_graph(PLEfemaleFlower_specific_goterms)
 b<-plot_go_graph(PLEmaleFlower_specific_goterms)
 c<-plot_go_graph(PLEleaf_specific_goterms)
@@ -351,14 +312,13 @@ d<-plot_go_graph(PLEpetiole_specific_goterms)
 e<-plot_go_graph(PLEroot_specific_goterms)
 f<-plot_go_graph(PLEvegBud_specific_goterms)
 
-
+# check how long each Go term list is for tables in manuscript
 length(CONfemaleFlower_specific_goterms)
 length(CONmaleFlower_specific_goterms)
 length(CONleaf_specific_goterms)
 length(CONpetiole_specific_goterms)
 length(CONroot_specific_goterms)
 length(CONvegBud_specific_goterms)
-
 
 length(PLEfemaleFlower_specific_goterms)
 length(PLEmaleFlower_specific_goterms)
@@ -367,7 +327,8 @@ length(PLEpetiole_specific_goterms)
 length(PLEroot_specific_goterms)
 length(PLEvegBud_specific_goterms)
 
-
+# arrange all figures together
+# to go in supplemental due to size
 figure <- ggarrange(a, b, c, d, e, f,
                     labels = c("A", "B", "C", "D", "E", "F"),
                     ncol = 3, nrow = 2)
@@ -505,15 +466,37 @@ test<-data.frame(CON_TRINITY_DN7494_c0_g2_i3 = c(1079.21105353443,	1355.12934525
                  )
 
 test<-t(test)
-colnames(test) <- c("femaleFlower"	,"leaf",	"maleFlower"	,"petiole",	"root"	,"vegBud")
+colnames(test) <- c("Female flower"	,"Leaf",	"Male flower"	,"Petiole",	"Root"	,"Vegetative bud")
 
 
+transcript_ids<-c("CON_TRINITY_DN7494_c0_g2_i3", 
+                  "PLE_TRINITY_DN4929_c0_g1_i2", 
+                  "CON_TRINITY_DN7494_c0_g1_i13",
+                  "PLE_TRINITY_DN4929_c0_g5_i2", 
+                  "CON_TRINITY_DN7494_c0_g4_i1", 
+                  "PLE_TRINITY_DN4929_c0_g4_i3", 
+                  "CON_TRINITY_DN7494_c0_g5_i1", 
+                  "PLE_TRINITY_DN9097_c0_g2_i1")
+
+
+
+meta<-data.frame(paralog_group = c(rep(c("group1"), 2), rep(c("group2"), 2), rep(c("group3"), 2), rep(c("group4"), 2)) )
+rownames(meta) <-transcript_ids
+meta_cols<-list(paralog_group = c(group1 = "lightblue3", group2 = "pink2", group3 = "lightgoldenrod2", group4 = "darkseagreen2"))
 
 pheatmap(test, cluster_rows = FALSE, scale = "column")
 pheatmap(test, cluster_rows = FALSE)
 
-pheatmap(log2(test), cluster_rows = FALSE)
-
+pheatmap(log2(test), 
+         cluster_rows = FALSE, 
+         fontsize = 13, 
+         angle_col = 45, 
+         fontsize_col = 17, 
+         fontsize_row = 15, 
+         annotation_row=meta, 
+         annotation_names_row = FALSE,
+         annotation_colors = meta_cols
+         )
 
 
 
@@ -581,8 +564,8 @@ ple_listInput<-list(blastx = ple_blastx_annot,
                     GOpfam = ple_GOpfam_annot)
 
 
-upset(fromList(con_listInput), order.by = "freq", nsets=6, text.scale = c(2, 2, 1.8, 1.8, 2, 1.9))
-upset(fromList(ple_listInput), order.by = "freq", nsets=6, text.scale = c(2, 2, 1.8, 1.8, 2, 1.9))
+upset(fromList(con_listInput), order.by = "freq", nsets=6, text.scale = c(2, 2, 1.8, 1.8, 2.5, 2.6))
+upset(fromList(ple_listInput), order.by = "freq", nsets=6, text.scale = c(2, 2, 1.8, 1.8, 2.5, 2.4))
 
 
 
